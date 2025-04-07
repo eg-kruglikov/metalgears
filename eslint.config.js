f12b958136cb33ab86bbe840d28adf44a73fd80c
Egor <eg.kruglikov@yandex.ru>
2025-04-07 19:10:05 +0300

    fix linter, add cname

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
+import eslintPluginImport from "eslint-plugin-import";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import eslintPluginImport from "eslint-plugin-import";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -19,6 +20,7 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       import: eslintPluginImport,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -39,6 +41,8 @@ export default tseslint.config(
           tsx: "always",
         },
       ],
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
     },
 
     settings: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
 function App() {
+  const [test, setTest] = useState(0);
+
+  useEffect(() => {
+    const setHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      if (vh > 0) {
+        setTest(vh);
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+      }
+    };
+
+    // Подождать немного перед первым вызовом
+    const timeout = setTimeout(() => {
+      setHeight();
+    }, 100);
+
+    window.addEventListener("resize", setHeight);
+
+    return () => {
+      clearTimeout(timeout);
+      window.removeEventListener("resize", setHeight);
+    };
+  }, []);
   return (
     <div className="scroll-container">
       <div className="section first">
-        <h1>Экран 1</h1>
+        <h1>{test}</h1>
       </div>
       <div className="section second">
-        <h1>Экран 2</h1>
+        <h1>{test}</h1>
       </div>
     </div>
   );
